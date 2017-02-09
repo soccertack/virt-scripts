ecef1ccb9958a6a74e9aac5b8d6df90b678669c3
jintackl <jintackl@kvm-node.jintackl-qv22040.kvmarm-pg0.apt.emulab.net>
2017-02-08 17:50:42 -0700

    Add Peter's qemu repo. Don't install libvirt

--- libvirt_qemu_update.sh
+++ libvirt_qemu_update.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 cd /srv/vm/qemu
 git remote add qemu git://git.qemu.org/qemu.git
+git remote add peter https://github.com/xzpeter/qemu.git
 git fetch qemu
-git checkout v2.8.0
+git fetch peter
+git checkout vtd-vfio-enablement-v7
+./configure --target-list=x86_64-softmmu
+# If this file is missing, then make install gives an error
+touch trace-events-all
 make -j 20
 make install
  
+# Just install qemu for now
+exit
 cd ~
 git clone https://github.com/libvirt/libvirt.git -b v3.0.0
 apt-get update
