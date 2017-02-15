edfc94b209d312bc52d043bb3947f3fedf78dd63
Jintack Lim <jintack@cs.columbia.edu>
2017-02-15 15:24:31 -0700

    Add vfio preparation script for L2

--- prepare-vfio-l1.sh
+++ prepare-vfio-l1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 modprobe vfio-pci
-ls /sys/bus/pci/devices/0000\:08\:00.0/driver/unbind
+ls /sys/bus/pci/devices/0000\:00\:03.0/driver/unbind
 echo "Unbind device"
-echo 0000:08:00.0 | sudo tee /sys/bus/pci/devices/0000\:08\:00.0/driver/unbind
+echo 0000:00:03.0 | sudo tee /sys/bus/pci/devices/0000\:00\:03.0/driver/unbind
 echo "Bind to vfio-pci"
 echo 15b3 1003 | tee /sys/bus/pci/drivers/vfio-pci/new_id
 echo "Check"
